d683da8ec850b8a6bd10324c7d7eab4aa15d9772
GustavoTiburcio <gustavotiburcio23@gmail.com>
2021-08-26 10:58:10 -0300

    Finalizada tela de visualizar estoque

--- AppEstoque.js
+++ AppEstoque.js
@@ -7,67 +7,81 @@ export default function AppEstoque({ route, navigation }) {
 
     const codbar = route.params?.codbar;
     let teste;
-    const [data, setData] = useState([]);
+    const [data, setData] = useState({});
     
     async function getListarEstoque(){
       const response = await api.get(`/mercador/listarParaDetalhes?codbar=${codbar}`)
-      teste = response.data.estest1
-      console.log(teste)
+      setData(response.data)
     }
     
     useEffect(()=>{
     getListarEstoque();
     },[])
+
+    useEffect(() => {
+      console.log(data)
+    },[data])
     
   return (
     <View style={styles.container}>
-      <Text>Estoque</Text>
-      <Grid>
-        <Col size={50}>
-          <Row style={styles.cell}>
-            <Text>Matriz</Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>Andre</Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>Alexandre</Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>Fabio</Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>Cilas</Text>
-          </Row>
-        </Col>
-        <Col size={25}>
-        <Row style={styles.cell}>
-            <Text>{String(teste)}</Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>F</Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>G</Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>H</Text>
-          </Row>
-          <Row style={styles.cell}>
-            <Text>I</Text>
-          </Row>
-        </Col>
-      </Grid>
+      <Text style={{textAlign: 'center', fontSize: 24, color:'#000000', paddingTop: 10}}>Estoque</Text>
+      <View style={styles.tableView}>
+        <Grid>
+          <Col size={50}>
+            <Row style={styles.cell}>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>Matriz</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>Andre</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>Alexandre</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>Fabio</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>Cilas</Text>
+            </Row>
+          </Col>
+          <Col size={25}>
+            <Row style={styles.cell}>
+              <Text>Quantidade</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>{data.estest1}</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>{data.estest2}</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>{data.estest3}</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>{data.estest4}</Text>
+            </Row>
+            <Row style={styles.cell}>
+              <Text>{data.estest5}</Text>
+            </Row>
+          </Col>
+        </Grid>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  tableView: {
     width: '100%',
     height: 300,
     padding: 16,
-    paddingTop: 100,
+    paddingTop: 50,
     backgroundColor: '#fff',
   },
   cell: {
