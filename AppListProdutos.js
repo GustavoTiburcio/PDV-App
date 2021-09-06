64d42be9cbe91aacd13ff25ec55820038344adfa
GustavoTiburcio <gustavotiburcio23@gmail.com>
2021-09-06 11:37:49 -0300

    Ajustando imagem dos produtos

--- AppListProdutos.js
+++ AppListProdutos.js
@@ -93,7 +93,7 @@ function ListItem( {data} ){
   return(
     <View style={styles.listItem}>
       <Image
-         style={styles.imagemDosProdutos}
+         style={{width: '100%', height: 170}}
          source={{
          uri: foto(data.linkFot),
         }}
