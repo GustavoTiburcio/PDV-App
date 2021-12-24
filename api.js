6afa288850b3d758a50be570e0f5bf7a0e960101
GustavoTiburcio <gustavotiburcio23@gmail.com>
2021-12-24 08:59:23 -0300

    new branch

--- api.js
+++ api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-      //baseURL: 'http://192.168.25.167:8089/api' //Local TiFire
-      //baseURL: 'https://goldchaves-api.herokuapp.com/api' //Produção Heroku
-      baseURL: 'http://tifire.sytes.net:8089/api'  //Produção Servidor TiFire
+      baseURL: 'https://guizzi-api.herokuapp.com/api' //Produção Heroku
 });
 
 export default api;
