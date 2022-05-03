72fbd8cc1118bccc269bd18d66da34d8983797ff
GustavoTiburcio <gustavotiburcio23@gmail.com>
2022-05-03 17:02:24 -0300

    Corrigido endereço da API

--- api.js
+++ api.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-      //baseURL: 'http://192.168.25.167:8089/api' //Local TiFire
-      //baseURL: 'http://tifire.sytes.net:8089/api'  //Produção Servidor TiFire
-      baseURL: 'https://goldchavesheroku.herokuapp.com/api'  //Heroku Provisório
+      //baseURL: 'http://192.168.25.167:8087/api' //Local TiFire
+      baseURL: 'http://tifire.sytes.net:8087/api'  //Produção Servidor TiFire
+      //baseURL: 'https://goldchavesheroku.herokuapp.com/api'  //Heroku Provisório
 });
 
 export default api;
