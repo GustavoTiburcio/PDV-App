65caec9e481984ffe0fb79172e8935cb16be80f1
GustavoTiburcio <gustavotiburcio23@gmail.com>
2022-07-18 14:43:10 -0300

    Branch backup Gold

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import { Text, View, Button} from 'react-native';
 export default function Home({ navigation }) {
   return (
     <View>
-      <Text> Home </Text>
+      <Text>Home</Text>
       <Button title="Perfil"
       onPress={() => navigation.navigate('AppCarrinho')}
       />
